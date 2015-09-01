dc18b8db7bef7f1d88ae5004e3889da36b0b3df3
l34p <hmg0228@gmail.com>
2015-09-02 01:45:34 +0900

    delete dummy

--- static/templates/account/calm-login/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/Lewitje/pen/BNNJjo.
-
- A ultra simple login screen on a calm breezy day,
--- static/templates/account/calm-login/js/index.js
+++
@@ -1,6 +0,0 @@
- $("#login-button").click(function(event){
-		 event.preventDefault();
-	 
-	 $('form').fadeOut(500);
-	 $('.wrapper').addClass('form-success');
-});
